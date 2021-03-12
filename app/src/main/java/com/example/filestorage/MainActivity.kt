78c3745db95538a489a2217cb24767f4ea9b177a
shoaib <mohshoaibkhan0@gmail.com>
2021-03-13 00:19:37 +0530

    first commit

--- app/src/main/java/com/example/filestorage/MainActivity.kt
+++ app/src/main/java/com/example/filestorage/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val file = "file.tx"
+        val file = "file.txt"
 
         btnWrite.setOnClickListener(){
             if(editText.text.toString().isBlank()){
